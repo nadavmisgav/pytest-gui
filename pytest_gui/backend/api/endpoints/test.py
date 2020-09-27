7efbb0fefba9e84078cf00612a5be0b28f034cd5
ned3144 <nadav.misgav@gmail.com>
2020-09-27 18:52:04 +0300

    fix swagger

--- pytest_gui/backend/api/endpoints/test.py
+++ pytest_gui/backend/api/endpoints/test.py
@@ -1,16 +1,29 @@
 from pytest_gui.pytest.pytest_wrapper import worker
 
 def select(test):
-    module = test["module"]
-    name = test["name"]
+    module = test.get("module", None)
+    name = test.get("name", None)
     selected = test["selected"]
+    found_idx = 0
     
-    for idx, test in enumerate(worker.modules[module]):
-        if test["name"] == name:
-            worker.modules[module][idx]["selected"] = selected
+    if module == None: # All modules
+        for m in worker.modules:
+            for idx, test in enumerate(worker.modules[m]):
+                worker.modules[module][idx]["selected"] = selected
+    else:
+        for idx, test in enumerate(worker.modules[module]):
+            # Filter by name or name == None - all tests
+            if test["name"] == name or name == None:
+                worker.modules[module][idx]["selected"] = selected
+                found_idx = idx
+                
+    return worker.modules
 
 def get():
     return worker.modules
+
+def run():
+    pass
             
     
     
