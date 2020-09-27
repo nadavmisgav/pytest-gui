8928610f6e5e605a152026114d53101699e9f3ee
ned3144 <nadav.misgav@gmail.com>
2020-09-27 11:13:17 +0300

    added get_markers

--- pytest_gui/backend/api/endpoints/markers.py
+++ pytest_gui/backend/api/endpoints/markers.py
@@ -1,7 +1,8 @@
+
 from pytest_gui.pytest.pytest_wrapper import worker
 
 def get():
-    worker.discover()
-    return worker.tests
+    worker.get_markers()
+    return worker.markers
     
     
--- pytest_gui/pytest/pytest_wrapper.py
+++ pytest_gui/pytest/pytest_wrapper.py
@@ -7,9 +7,40 @@ from multiprocessing.connection import Listener
 from collections import defaultdict
 from decouple import config
 
+from pytest_gui.backend.config import DEBUG, TEST_DIR
+
 PLUGIN_PORT = config("PYTEST_GUI_PLUGIN_PORT", cast=int, default=6000)
 PLUGIN_PATH = "pytest_gui.pytest.pytest_gui_plugin"
+ADDRESS = ('localhost', PLUGIN_PORT)
 
+
+_builtin_markers = [
+    "no_cover",
+    "filterwarnings",
+    "skip",
+    "skipif",
+    "xfail",
+    "parametrize",
+    "usefixtures",
+    "tryfirst",
+    "trylast",
+]
+def _filter_only_custom_markers(out):
+    """Generator for parse output and return custom markers
+    Args:
+        out (str): output of pytest --markers
+    Yields:
+        tuple(str, str): contains name and description of the marker
+    """
+    for marker in out:
+        if marker.startswith("@"):
+            name = marker.split(":")[0].split(".")[2]
+            desc = "".join(marker.split(":")[1:]).strip().rstrip(".")
+            if any(name.startswith(marker) for marker in _builtin_markers):
+                continue
+            yield name, desc
+
+        
 def generate_messages(conn):
     while True:
         msg = conn.recv()
@@ -25,19 +56,21 @@ class PytestWorker:
         self.tests = None
         self.markers = None
     
-    def discover(self, filter=""):
-        address = ('localhost', PLUGIN_PORT)
-        listener = Listener(address)
+    def discover(self):
+        # TODO: Handle errors in collect
+        listener = Listener(ADDRESS)
         
         p = self._run_pytest(self.test_dir, "--collect-only")
         conn = listener.accept()
         # TODO: add logging
         print('connection accepted from', listener.last_accepted)
-        tests = json.loads(next(generate_messages(conn))) # Only one message
-        listener.close()
+        
+        try:
+            tests = json.loads(next(generate_messages(conn))) # Only one message
+        finally:
+            listener.close()
         
         self.tests = self._parse_discover(tests["tests"]) 
-        # TODO: Handle errors in collect
         
     @staticmethod
     def _parse_discover(tests):
@@ -49,15 +82,21 @@ class PytestWorker:
             modules[module].append({"name": name, "line": line, "selected": True})
         return modules
         
-  
-    def markers(self, filter=""):
-        pass
-    
+    def get_markers(self):
+        # TODO: Handle errors in markers
+        p = self._run_pytest(self.test_dir, "--markers")
+        self.markers = [{"name": name, "description": desc} for name, desc in _filter_only_custom_markers(p.stdout)]
+            
     @staticmethod
     def _run_pytest(*args):
         return subprocess.Popen(['pytest', "-p", PLUGIN_PATH] + list(args), 
                          stdout=subprocess.PIPE, 
                          universal_newlines=True)     
 
-a = PytestWorker("/home/nadav/projects/calc_test/tests")
-a.discover()
+
+worker = PytestWorker(TEST_DIR)
+
+if __name__ == "__main__":
+    a = PytestWorker("/home/nadav/projects/calc_test/tests")
+    a.get_markers()
+    print(a.markers)
