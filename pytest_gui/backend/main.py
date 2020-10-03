5b78dc2f74b17a74544d19ede0f3148d121050cf
ned3144 <nadav.misgav@gmail.com>
2020-10-03 13:02:29 +0300

    setup production and logger

--- pytest_gui/backend/main.py
+++ pytest_gui/backend/main.py
@@ -1,26 +1,40 @@
-import sys
 import os
+import sys
 import connexion
+import logging
+
+from gevent.pywsgi import WSGIServer
 from decouple import config
 
 
 DEBUG = config("PYTEST_GUI_DEBUG", cast=bool, default=False)
 SERVER_PORT = config("PYTEST_GUI_PORT", cast=int, default=5000)
+HOST = "localhost"
 
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'api/endpoints')))
-app = connexion.App(__name__, specification_dir='./api/')
+app = connexion.FlaskApp(__name__, specification_dir='./api/')
 app.add_api('swagger.yaml')
+
+# Set logger
+logger = app.app.logger
+logger.setLevel(logging.DEBUG if DEBUG else logging.INFO)
+logger.handlers[0].setFormatter(logging.Formatter('[%(asctime)s]::%(levelname)s::%(message)s'))
+
+
+http_server = WSGIServer((HOST, SERVER_PORT), app)
+
 
 @app.route('/')
 def react_app():
     return "Hello world"
 
+
 def cmd(argv=sys.argv):
-    # if len(argv) != 2:
-    #     print("usage pytest-gui test_dir")
-    app.run(host='0.0.0.0', port=SERVER_PORT, debug=DEBUG)
+    logger.info(f"Starting Pytest-GUI app on {HOST}:{SERVER_PORT} [DEBUG={DEBUG}]")
+    http_server.serve_forever()
+    
     
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=SERVER_PORT, debug=DEBUG)
+    cmd()
