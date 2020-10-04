0e3ef51083c6fb26a29d00a14ce4263f3a294a81
ned3144 <nadav.misgav@gmail.com>
2020-10-04 17:07:01 +0300

    change select api

--- pytest_gui/backend/api/endpoints/test/test.py
+++ pytest_gui/backend/api/endpoints/test/test.py
@@ -1,25 +1,9 @@
 from pytest_gui.pytest.pytest_wrapper import worker
 
 
-def select(test):
-    # TODO: fix modules use case
-    module = test.get("module", None)
-    name = test.get("name", None)
-    selected = test["selected"]
-    found_idx = 0
-
-    if module is None:  # All modules
-        for m in worker.modules:
-            for idx, test in enumerate(worker.modules[m]):
-                worker.modules[module][idx]["selected"] = selected
-        return worker.modules
-    else:
-        for idx, test in enumerate(worker.modules[module]):
-            # Filter by name or name == None - all tests
-            if test["name"] == name or name is None:
-                worker.modules[module][idx]["selected"] = selected
-                found_idx = idx
-        return worker.modules[found_idx]
+def select(tests):
+    worker.tests = tests
+    return worker.tests
 
 
 def get():
